e3ed51239b85296f16d74a43ae14d37333705fb3
tchiinhemba <tchiinhemba@gmail.com>
2026-07-13 09:09:14 +0100

    change endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "https://api-bt.bisc8.digital",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api(\/v1)?/, "/api/v1"),
       },
